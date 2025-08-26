66e9af1267190fc8d9a723b8b6d53cfea756bfb1
Habib Shamun <habibsalehinshamun@gmail.com>
2025-08-26 22:38:44 +0600

    added another empty  html file and did the login page js

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("loginButton").addEventListener('click',function(e){
+    e.preventDefault();
+const mobileNumber=12345678910;
+const pinNumber=1234;
+const mobileNumberValue=document.getElementById("mobile-number").value;
+const mobileNumberValueConverted=parseInt(mobileNumberValue);
+const pinNumberValue=document.getElementById("pin-number").value;
+const pinNumberValueConverted=parseInt(pinNumberValue);
+
+if(mobileNumberValueConverted===mobileNumber && pinNumberValueConverted===pinNumber) {
+    console.log("All values matched");
+} else {
+    console.log("Invalid Credentials")
+}
+})
